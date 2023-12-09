7993109ced54a0de1f0bcd64b72d2dd51567aab8
Sudara <sudara@alonetone.com>
2023-12-10 00:56:33 +0100

    Add stroked path tests placeholder

---
+++ tests/stroked_path.cpp
@@ -0,0 +1,13 @@
+#include "../melatonin/implementations/gin.h"
+#include "../melatonin/shadows.h"
+#include "helpers/pixel_helpers.h"
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+
+// All of these tests operate @1x
+TEST_CASE ("Melatonin Blur Stroked Path")
+{
+
+
+}
