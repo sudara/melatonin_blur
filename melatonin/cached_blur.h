3f6e6c2e54ddd2df19efbf3607a5b7d871a77650
Sudara <sudara@modernthings.net>
2024-07-31 22:53:41 +0200

    Improvements to cached blur (#69)

--- melatonin/cached_blur.h
+++ melatonin/cached_blur.h
@@ -7,19 +7,25 @@ namespace melatonin
     public:
         explicit CachedBlur (size_t r);
 
-        // we are passing the source by value here
+        // we are passing the source by reference here
         // (but it's a value object of sorts since its reference counted)
         void update (const juce::Image& newSource);
 
-        juce::Image& render (juce::Image& newSource);
+        // Render and potentially update the image
+        juce::Image& render (const juce::Image& newSource);
+
+        // Render the image from cache
         juce::Image& render();
+
+        void setRadius (size_t newRadius);
 
     private:
         // juce::Images are value objects, reference counted behind the scenes
         // We want to store a reference to the src so we can compare on render
         // And we actually are the owner of the dst
         size_t radius = 0;
-        juce::Image src;
-        juce::Image dst;
+        juce::Image src {};
+        juce::Image dst {};
+        bool needsRedraw = false;
     };
 }
